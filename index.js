64f24d53a6c447cde390d3ea1661e60d2dbff2dc
Lo r <lorelle.reid@gmail.com>
2019-03-07 06:39:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = `${conversation} space`;
+var topic = `${conversation}space`;
 
 var conversation = `${topic}`;
